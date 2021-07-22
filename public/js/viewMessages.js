7ed585cd6fcee29a06f79bcbe668bb236781d6f0
“Nuha” <nuha14m@gmail.com>
2021-07-22 17:33:29 +0000

    Added modal to view secret message.

--- public/js/viewMessages.js
+++ public/js/viewMessages.js
@@ -4,15 +4,20 @@ const getMessages = () => {
     
     messagesRef.on('value', (snapshot)=> {
         const data = snapshot.val()
-        const passcode = document.querySelector("#passcode").value.toLowerCase();
+        const passcode_obj = document.querySelector("#passcode");
+        const passcode = passcode_obj.value.toLowerCase();
         const keys = Object.keys(data);
         if(keys.indexOf(passcode)>-1){
             document.querySelector("#message").innerHTML= data[passcode];
+            document.querySelector("#modal").classList.add("is-active");  
         }
         else{
             document.querySelector("#message").innerHTML= "";
             alert("Passcode does not exist!")
         }
-        
     })
+}
+
+const closeModal = () => {
+      document.querySelector("#modal").classList.remove("is-active");
 }
